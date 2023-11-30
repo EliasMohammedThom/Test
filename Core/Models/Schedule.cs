832c78ff32cc1deffd6dda19ded5887035394b1d
Elias Zanghnaeh <teachereli@outlook.com>
2023-11-30 10:33:53 +0100

    userid is now nullable in schedules

--- Core/Models/Schedule.cs
+++ Core/Models/Schedule.cs
@@ -9,7 +9,7 @@ namespace Core.Models
     public class Schedule
     {
         public int Id { get; set; }
-        public string UserId { get; set; }
+        public string? UserId { get; set; }
         public string? Title { get; set; }
 
     }
