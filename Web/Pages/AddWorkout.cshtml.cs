b3b9eaf6bac7d3bf49f7a3daf1dbc73134135ff3
thomf20 <thom.flodqvist@gmail.com>
2023-12-05 20:44:49 +0100

    Added  small changes

--- Web/Pages/AddWorkout.cshtml.cs
+++ Web/Pages/AddWorkout.cshtml.cs
@@ -11,35 +11,31 @@ namespace Web.Pages
     {
         private readonly IWorkoutService _workoutService;
 
-
         [BindProperty]
         public Workout workout { get; set; } = default!;
 
         [BindProperty]
         public string ReplyToUser { get; set; } = default!;
-
         
         public AddWorkoutModel(IWorkoutService workoutService)
         {
             _workoutService = workoutService;
         }
-
       
         public async Task<IActionResult> OnPostAsync()
         {
-
             var listofworkouts = _workoutService.GetWorkoutsByTitle(workout.Title);
 
             if(listofworkouts.Count >= 1 ) 
             {
                 ReplyToUser = "This Workouts name already exists, please choose something else";
+                TempData["ErrorMessage"] = ReplyToUser;
             }
             else
             {
                 _workoutService.AddWorkout(workout);
+                ReplyToUser = "Workout successfully added";
             }
-            
-            
             return Page();
         }
     }
