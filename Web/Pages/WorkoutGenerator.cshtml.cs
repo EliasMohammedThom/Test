94f552add96d1e5f1f965aa3344deaf0b2abd464
Elias Zanghnaeh <teachereli@outlook.com>
2024-04-12 14:32:15 +0200

    i did it

--- Web/Pages/WorkoutGenerator.cshtml.cs
+++ Web/Pages/WorkoutGenerator.cshtml.cs
@@ -107,6 +107,8 @@ namespace Web.Pages
                 1,2,3,4,5,6,7,8,9,10,11,12,13,14
             };
 
+            Placeholder = new();
+
         }
         public async Task<IActionResult> OnGetAsync()
         {
