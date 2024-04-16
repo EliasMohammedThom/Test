365de15ffffe5de7900a0e32cb5b6ef7d2b52d61
Mohammed Karimi <mohammed.karimi01@hotmail.com>
2024-04-16 11:41:28 +0200

    test

---
+++ Core/Models/ListValue.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class ListValue
+    {
+        public class DifficultyCategory
+        {
+            public List<string> Difficulty { get; set; }
+        }
+        public class WorkoutEquipment
+        {
+            public List<string> Equipment { get; set; }
+        }
+        public class WorkoutType
+        {
+            public List<string> Type { get; set; }
+        }
+        public class MuscleCategory
+        {
+            public List<string> Muscle { get; set; }
+        }
+        public class AmountOfWorkout
+        {
+            public List<int> Amount { get; set; }
+        }
+        public class AmountOfExercise
+        {
+            public List<int> Amount { get; set; }
+        }
+
+
+        public static List<DifficultyCategory> DifficultyCategories { get; set; }
+        public static List<WorkoutEquipment> WorkoutEquipments { get; set; }
+        public static List<WorkoutType> WorkoutTypes { get; set; }
+        public static List<MuscleCategory> MuscleCategories { get; set; }
+        public static List<AmountOfWorkout> AmountOfWorkouts { get; set; }
+        public static List<AmountOfExercise> AmountOfExercises { get; set; }
+
+
+        static ListValue()
+        {
+
+            DifficultyCategories = new List<DifficultyCategory>
+            {
+                new DifficultyCategory
+                {
+                    Difficulty = new List<string>
+                    {
+                        "Beginner",
+                        "Intermediate",
+                        "Expert"
+                    }
+                }
+            };
+
+            WorkoutEquipments = new List<WorkoutEquipment>
+            {
+                new WorkoutEquipment
+                {
+                    Equipment = new List<string>
+                    {
+                        "Body_only",
+                        "Barbell",
+                        "Other",
+                        "None",
+                        "Dumbbell",
+                        "Machine",
+                        "Cable",
+                        "Kettlebells",
+                        "Bands",
+                        "Foam_roll",
+                        "Exercise_ball",
+                        "Medicine_ball",
+                        "E-z_curl_bar",
+                    }
+                }
+            };
+
+            WorkoutTypes = new List<WorkoutType>
+            {
+                new WorkoutType
+                {
+                    Type = new List<string>
+                    {
+                        "Cardio",
+                        "Olympic_weightlifting",
+                        "Plyometrics",
+                        "Powerlifting",
+                        "Strength",
+                        "Stretching",
+                        "Strongman"
+                    }
+                }
+            };
+            MuscleCategories = new List<MuscleCategory>
+            {
+                new MuscleCategory
+                {
+                    Muscle = new List<string>
+                    {
+                        "Abdominals",
+                        "Abductors",
+                        "Adductors",
+                        "Biceps",
+                        "Calves",
+                        "Chest",
+                        "Forearms",
+                        "Glutes",
+                        "Hamstrings",
+                        "Lats",
+                        "Lower_back",
+                        "Middle_back",
+                        "Neck",
+                        "Quadriceps",
+                        "Traps",
+                        "Triceps"
+                    }
+                }
+            };
+
+            AmountOfWorkouts = new List<AmountOfWorkout>
+            {
+                new AmountOfWorkout
+                {
+                    Amount = new List<int>
+                    {
+                        1,2,3,4,5,6,7,8,9,10,11,12,13,14
+                    }
+                }
+            };
+            AmountOfExercises = new List<AmountOfExercise>
+            {
+                new AmountOfExercise
+                {
+                    Amount = new List<int>
+                    {
+                         1,2,3,4,5,6,7,8,9,10
+                    }
+                }
+            };
+        }
+    }
+}
--- Web/Pages/WorkoutGenerator.cshtml.cs
+++ Web/Pages/WorkoutGenerator.cshtml.cs
@@ -5,7 +5,6 @@ using Infrastructure.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System;
 using System.Reflection.PortableExecutable;
 
 namespace Web.Pages
@@ -17,26 +16,29 @@ namespace Web.Pages
         private readonly UserManager<IdentityUser> _userManager;
 
 
-        [BindProperty]
-        public List<string> DifficultyCategory { get; set; }
+        //[BindProperty]
+        //public List<string> DifficultyCategory { get; set; }
 
-        [BindProperty]
-        public List<string> WorkoutEquipment { get; set; }
+        //[BindProperty]
+        //public List<string> WorkoutEquipment { get; set; }
 
-        [BindProperty]
-        public List<string> WorkoutType { get; set; }
+        //[BindProperty]
+        //public List<string> WorkoutType { get; set; }
 
-        [BindProperty]
-        public List<string> MuscleCategories { get; set; }
+        //[BindProperty]
+        //public List<string> MuscleCategories { get; set; }
 
-        [BindProperty]
-        public List<int> AmountOfWorkouts { get; set; }
-        [BindProperty]
-        public List<int> AmountOfExercises { get; set; }
+        //[BindProperty]
+        //public List<int> AmountOfWorkouts { get; set; }
+        //[BindProperty]
+        //public List<int> AmountOfExercises { get; set; }
 
 
+<<<<<<< Updated upstream
         public List<FetchedExercises> GeneratedExercises { get; set; } = default!;
 
+=======
+>>>>>>> Stashed changes
         [BindProperty]
         public InputValues Placeholder { get; set; }
 
@@ -45,14 +47,10 @@ namespace Web.Pages
         //public IdentityUser? IdentityUser { get; set; }
         public Schedule Schedule { get; set; }
 
-
         [BindProperty]
         public string ErrorMessage { get; set; }
 
-        public Workout Workout { get; set; }
-
-
-
+        public ListValue Listvalues { get; set; }
 
         public WorkoutGeneratorModel(ApplicationDbContext applicationDbContext, IScheduleService scheduleService, UserManager<IdentityUser> userManager)
         {
@@ -61,89 +59,25 @@ namespace Web.Pages
             _userManager = userManager;
 
 
-            DifficultyCategory = new List<string>
-            {
-                    "Beginner",
-                    "Intermediate",
-                    "Expert"
-            };
-
-            WorkoutEquipment = new List<string>
-            {
-                "Body_only",
-                "Barbell",
-                "Other",
-                "None",
-                "Dumbbell",
-                "Machine",
-                "Cable",
-                "Kettlebells",
-                "Bands",
-                "Foam_roll",
-                "Exercise_ball",
-                "Medicine_ball",
-                "E-z_curl_bar",
-            };
-
-            WorkoutType = new List<string> {
-                        "Cardio",
-                        "Olympic_weightlifting",
-                        "Plyometrics",
-                        "Powerlifting",
-                        "Strength",
-                        "Stretching",
-                        "Strongman"
-                    };
-
-            MuscleCategories = new List<string> {
-                        "Abdominals",
-                        "Abductors",
-                        "Adductors",
-                        "Biceps",
-                        "Calves",
-                        "Chest",
-                        "Forearms",
-                        "Glutes",
-                        "Hamstrings",
-                        "Lats",
-                        "Lower_back",
-                        "Middle_back",
-                        "Neck",
-                        "Quadriceps",
-                        "Traps",
-                        "Triceps"
-                    };
-
-            AmountOfWorkouts = new List<int>
-            {
-                1,2,3,4,5,6,7,8,9,10,11,12,13,14
-            };
-
-            AmountOfExercises = new List<int>
-            {
-                1,2,3,4,5,6,7,8,9,10
-            };
-
             Placeholder = new();
-
-            Workout = new Workout();
-            GeneratedExercises = new();
-        
-            Workout.Date = DateOnly.FromDateTime(DateTime.Now);
 
         }
         public async Task<IActionResult> OnGetAsync()
         {
+            Listvalues = new();
+
+            //IdentityUser = await _userManager.GetUserAsync(User);
+
+
             return Page();
         }
         public async Task OnPost()
         {
-
             IdentityUser? identityUser = await _userManager.GetUserAsync(User);
 
             var doesScheduleExists = _scheduleService.GetScheduleByUserId(identityUser.Id);
 
-            var sortedExercises =
+            var sortedTestList =
                 _ApplicationDbContext.ExerciseLists.Where(
                 x =>
                 x.Difficulty == Placeholder.DifficultyCategory &&
@@ -154,24 +88,31 @@ namespace Web.Pages
                 if (sortedExercises.Count > 0 )
                 {
                 Workout.UserId = identityUser.Id;
-                Workout.ScheduleId = _scheduleService.GetScheduleByUserId(identityUser.Id).Id;
                 _ApplicationDbContext.Workouts.Add(Workout);
-
                 _ApplicationDbContext.SaveChanges();
 
 
+<<<<<<< Updated upstream
                 foreach ( var exercise in sortedExercises )
                 {
                     var fetchedExercise = new FetchedExercises
+=======
+            if (sortedTestList.Count > 0)
+            {
+>>>>>>> Stashed changes
 
+                foreach (var test in sortedTestList)
+                {                   
+                    _ApplicationDbContext.GeneratedExercises.Add(new GeneratedExercises
                     {
-                        Difficulty = exercise.Difficulty,
-                        Equipment = exercise.Equipment,
-                        Muscle = exercise.Muscle,
-                        Type = exercise.Type,
-                        Instructions = exercise.Instructions,
-                        Name = exercise.Name,
+                        Difficulty = test.Difficulty,
+                        Equipment = test.Equipment,
+                        Muscle = test.Muscle,
+                        Type = test.Type,
+                        Instructions = test.Instructions,
+                        Name = test.Name,
                         UserId = identityUser.Id
+<<<<<<< Updated upstream
                     };
 
                     GeneratedExercises.Add(fetchedExercise);
@@ -186,7 +127,12 @@ namespace Web.Pages
                 {
                     exercise.WorkoutId = Workout.Id;
                 }
+=======
+                        
+                    });;
+>>>>>>> Stashed changes
                 }
+        }
                 
 
             if (sortedExercises.Count == 0 || sortedExercises == null)
@@ -204,7 +150,7 @@ namespace Web.Pages
 
 
             }
-            if(sortedExercises.Count != 0)
+            if(sortedTestList.Count != 0)
             {
                 _ApplicationDbContext.InputValues.Add(Placeholder);
 
